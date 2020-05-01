60650517e157fb045ef6f4340c12d4aa13f77256
Yujibuu <yuujibuu@gmail.com>
2020-05-01 22:59:44 +0900

    fix directry

--- bot/itunes-topsong.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dirname="/home/yuji/workspace/itunes-topsong-rss"
-mkdir -p $dirname
-filename="${dirname}/hourly-topsong-`date +'%Y%m%d%H%M'`.xml"
-echo "Save to $filename"
-curl -s -o $filename -H "User-Agent: Crawlbot; yuujibuu@gmail.com" https://itunes.apple.com/jp/rss/topsongs/limit=10/xml
--- my-first-shell/my-first.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-ls
-date
-echo メッセージを入力してください。
-read message
-echo 入力されたメッセージ：$message
